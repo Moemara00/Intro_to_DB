4ced8b255376affb41e4e21a852d3844f782d1d3
Mohammed Emara <moemara034@gmail.com>
2024-10-20 21:42:21 +0300

    Magical Database for Your Dream Online Bookstore Adventure!

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,7 +31,7 @@ order_date DATE
 CREATE TABLE Order_Details(
 orderdetailid  PRIMARY Key
 order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id)
-book_id FOREIGN KEY (book_id) REFERENCES Books(book_id)
+book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id)
 quantity DOUBLE
 )
 
