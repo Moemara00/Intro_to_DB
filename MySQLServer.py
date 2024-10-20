23237e27db26c92f4cf2198d86189c76eca506df
Mohammed Emara <moemara034@gmail.com>
2024-10-20 21:54:43 +0300

    try2

--- MySQLServer.py
+++ MySQLServer.py
@@ -6,5 +6,5 @@ try :
     my_curssor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 except :
-    print("There is an error")
+    print(mysql.connector.DatabaseError)
 
