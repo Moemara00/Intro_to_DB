4cea923d8e628ba943da934e85026aad6a9246d0
Mohammed Emara <moemara034@gmail.com>
2024-10-20 23:00:13 +0300

    GO

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,22 @@
 
 
-SELECT *
+SELECT 
+
+    book_id,
+    PRIMARY KEY
+
+    title,
+    VARCHAR(130)
+
+    author_id,
+    (Foreign Key referencing Authors table)
+
+    price,
+    DOUBLE
+    
+    publication_date,
+    DATE
+
+
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books' AND TABLE_SCHEMA = 'alx_book_store'
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store'
