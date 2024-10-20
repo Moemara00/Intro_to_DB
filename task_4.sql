cb338e91d840372323c85e89488928329256ca19
Mohammed Emara <moemara034@gmail.com>
2024-10-20 22:14:41 +0300

    first try1

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,12 @@
 USE alx_book_store
-SELECT * FROM books
+
+CREATE TABLE Books(
+book_id PRIMARY KEY
+title VARCHAR (130)
+author_id (Foreign Key referencing Authors table)
+price DOUBLE
+publication_date DATE
+)
+
+SELECT * FROM Books
+
